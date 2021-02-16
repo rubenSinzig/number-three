3e2b1070df2733abcb211911481bd1cffe493796
rubenSinzig <ruben.sinzig@posteo.de>
2021-02-16 14:05:55 +0100

    Word Converter

---
+++ index.js
@@ -0,0 +1,19 @@
+// ##### 3. Word Converter
+//
+// - Given an array of words, create a function named "_wordConverter_" which uses an array method to add _"er"_ to the end of each string in the array.
+// Look at the expected output below. **Important**: Do **not** use a loop to complete this task.
+// Print your solution to the console.
+//
+// **Array of words**
+// ["smart", "kind", "sweet", "small", "clear"];
+//
+// **Expected output**
+// ["smarter", "kinder", "sweeter", "smaller", "clearer"];
+//
+console.log("------------- Solution One -------------");
+
+console.log("------------- Solution Two -------------");
+
+console.log("------------- Solution Three -------------");
+
+console.log("------------- Solution End -------------");
