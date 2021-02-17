8c12eb3621597ed0569fc11d9c0b05285bf16b85
rubenSinzig <ruben.sinzig@posteo.de>
2021-02-17 14:25:28 +0100

    Phone Number

--- README.md
+++
@@ -1,9 +0,0 @@
-# Number-Three
-
-## Three Assignments from the past in three different methods.
-
-1. **branch:** _wordConverter_ [Solutions](https://github.com/rubenSinzig/number-three/blob/wordConverter/index.js)
-
-3. **branch:**
-
-5. **branch:**
---
+++ index.js
@@ -0,0 +1,8 @@
+// Create a function that receive a text from user
+// and check if the user has entered a phone number, and print that out.
+// Note :
+// phone number should be within the German network starts with (030 or 017) only
+// and has 7 digits after the prefix number
+//  Examples :
+// > phone("Hi, call me on 030221A398 "); -> This is not a valid phone number 030221A398
+// > phone("Hi my number is 038  "); -> This is not a valid phone number 038
