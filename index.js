0c7af39cfb5b4fbc0e7f1887ef574cfa5ec4168c
rubenSinzig <ruben.sinzig@posteo.de>
2021-02-18 09:32:44 +0100

    Phone Number

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
 //  Examples :
 // > phone("Hi, call me on 030221A398 "); -> This is not a valid phone number 030221A398
 // > phone("Hi my number is 038  "); -> This is not a valid phone number 038
-const userStr = " Hi, call me on 0332214398  ";
+const userStr = " Hi, call me on 0302214398  ";
 console.log("------------ Solution One ------------");
-const checkPhoneNum = (str) => {
+const checkPhoneNumOne = (str) => {
   const strToArr = str.trim().toLowerCase().split(" ");
   const findPhoneNum = strToArr.find(
     (el) => el.slice(0, 3) == "030" || el.slice(0, 3) == "017"
@@ -19,7 +19,28 @@ const checkPhoneNum = (str) => {
     ? `${findPhoneNum} is your phone number.`
     : `${findPhoneNum} is NOT a valid phone number.`;
 };
-console.log(checkPhoneNum(userStr));
+console.log(checkPhoneNumOne(userStr));
 console.log("------------ Solution Two ------------");
+const checkPhoneNumTwo = (str) => {
+  const strToArr = str.trim().toLowerCase().split(" ");
+  let findPhoneNum = [];
+  for (let i = 0; i < strToArr.length; i++) {
+    if (
+      strToArr[i].substring(0, 3) == "030" ||
+      strToArr[i].substring(0, 3) == "017"
+    ) {
+      findPhoneNum.push(strToArr[i]);
+    }
+  }
+  let findNumToStr = findPhoneNum.join("");
+  if (findPhoneNum == undefined) {
+    return `You don't enter a phone number or your phone number starts not with (030 or 017).`;
+  } else if (findNumToStr.length == 10 && findNumToStr == +findPhoneNum) {
+    return `${findPhoneNum} is your phone number.`;
+  } else {
+    return `${findPhoneNum} is NOT a valid phone number.`;
+  }
+};
+console.log(checkPhoneNumTwo(userStr));
 console.log("------------ Solution Three ------------");
 console.log("------------ Solution End ------------");
